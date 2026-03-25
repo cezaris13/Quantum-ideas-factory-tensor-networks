c5bbb2c6eac10531cc8ea0037f1f10826170cf9c
cezaris13 <pijus.cezar13@gmail.com>
2026-03-25 15:51:13 +0100

    qft modifications

--- qft.py
+++ qft.py
@@ -1,30 +1,23 @@
-from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit, QuantumRegister
 import numpy as np
-import matplotlib.pyplot as plt
-from qiskit.quantum_info import Statevector, partial_trace, entropy
 
-n=15 #number_of_qubits
-def qft(noq):
-    qr = QuantumRegister(noq)
-    #cr = ClassicalRegister(noq)
-    qc = QuantumCircuit(qr)#QuantumCircuit(qr, cr)
-
-    for target in range(len(qr)): #from qubit zero to number of qubits
-
+def qft(n, t):
+    qr = QuantumRegister(n)
+    qc = QuantumCircuit(qr)
+    
+    t_curr = 0
+    for target in range(len(qr)): 
         qc.h(qr[target])
-        for control in range(target+1, noq):
+        for control in range(target+1, n):
             angle=np.pi/(2**(control-target))
             qc.cp(angle, qr[control], qr[target])
-
         
-    #swap afterwards
-    for i in range(noq//2):
-        qc.swap(qr[i], qr[noq-1-i])
+        if t_curr >= t:
+            return qc
 
-    #qc.measure(qr, cr)
+        t_curr +=1
+        
+    for i in range(n//2):
+        qc.swap(qr[i], qr[n-1-i])
 
     return qc
-
-qc = qft(n)
-
-qc.draw(output="mpl",style="clifford") #minimalistischer qc.draw()
